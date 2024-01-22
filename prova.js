be928afcf3ff22d99ea68cbf075bf5c0f21bf0f4
Mati <m.cotti1@campus.unimib.it>
2024-01-22 19:14:07 +0100

    Header + sistemato home_2

--- prova.js
+++ prova.js
@@ -2,10 +2,8 @@
 document.getElementById('mappa').addEventListener("mouseover", hovermappa);
 
 function hovermappa() {
-    document.getElementById("mappa_2").className = "d-block";
-    setTimeout(function() {
-        //Your code
-        document.getElementById("mappa_2").className = "d-none";
-    }, 3000);
+    document.getElementById('mappa').className='d-none';
+    document.getElementById('mappa_2').classList.replace("d-none", "d-block");
+    document.getElementById('blur').className='blur';
 };
 
