817af9628a3ef16515ca77460a8f8bf827adfe2d
Mati <m.cotti1@campus.unimib.it>
2024-01-23 15:03:33 +0100

    Sistemazione Pagine

--- prova.js
+++ prova.js
@@ -1,9 +1,11 @@
+$('#mappa img').on("mouseenter", function(){
+    $('#mappa').addClass('d-none');
+    $('#mappa_2').removeClass('d-none').addClass('d-block');
+    $('#blur').addClass('blur');
+});
 
-document.getElementById('mappa').addEventListener("mouseover", hovermappa);
-
-function hovermappa() {
-    document.getElementById('mappa').className='d-none';
-    document.getElementById('mappa_2').classList.replace("d-none", "d-block");
-    document.getElementById('blur').className='blur';
-};
-
+$('#bg_home2').on('click', function() {
+    $('#mappa').removeClass('d-none');
+    $('#mappa_2').addClass('d-none').removeClass('d-block');
+    $('#blur').removeClass('blur');
+});
