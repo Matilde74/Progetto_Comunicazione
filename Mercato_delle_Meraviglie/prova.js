3abfe64e246e52b8fd8126630c8ff1f8116a1dc4
Mati <m.cotti1@campus.unimib.it>
2024-01-26 14:38:48 +0100

    Pagina prodotto

--- Mercato_delle_Meraviglie/prova.js
+++ Mercato_delle_Meraviglie/prova.js
@@ -1,1 +1,14 @@
 $('.rombo').children().addClass('p_prodotto');
+$('.btn-rombo').children().addClass('p_prodotto');
+
+$('.seemore').on('click', function() {
+    $('p.base_text').clone().removeClass('base_text').appendTo('.descr_card');
+    $('.descr_card').addClass('d-block').removeClass('d-none');
+
+    $('.sfondo').on('click', function(){
+        $('p.base_text').addClass('base_text');
+        $('.descr_card').removeClass('d-block').addClass('d-none');
+    })
+
+});
+
