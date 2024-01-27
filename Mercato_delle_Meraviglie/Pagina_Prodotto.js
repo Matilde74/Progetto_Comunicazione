2f001aedbf29f6b7b5e735081bea1d46c8d83dc9
Mati <m.cotti1@campus.unimib.it>
2024-01-27 18:58:21 +0100

    Pagina Vestiti

--- Mercato_delle_Meraviglie/Pagina_Prodotto.js
+++ Mercato_delle_Meraviglie/Pagina_Prodotto.js
@@ -1,5 +1,14 @@
+$(document).ready(function(){
+    $.getJSON("prova.json", function(animale){
+        $('#nome').append(animale.name);
+    }).fail(function(){
+        console.log("An error has occurred.");
+    });
+});
+
 $('.rombo').children().addClass('p_prodotto');
-$('.btn-rombo').children().addClass('p_prodotto');
+$('.btn-rombo').children().addClass('p_prodotto')
+
 
 $('.seemore').on('click', function() {
     $('p.base_text').clone().removeClass('base_text').appendTo('.descr_card');
