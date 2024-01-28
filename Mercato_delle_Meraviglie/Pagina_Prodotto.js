d830c4d199ebebfadde7d899ed265f35638ded27
Mati <m.cotti1@campus.unimib.it>
2024-01-28 18:01:12 +0100

    Sistemazione Home_2

--- Mercato_delle_Meraviglie/Pagina_Prodotto.js
+++ Mercato_delle_Meraviglie/Pagina_Prodotto.js
@@ -1,17 +1,9 @@
-$(document).ready(function(){
-    $.getJSON("prova.json", function(animale){
-        $('#nome').append(animale.name);
-    }).fail(function(){
-        console.log("An error has occurred.");
-    });
-});
-
 $('.rombo').children().addClass('p_prodotto');
-$('.btn-rombo').children().addClass('p_prodotto')
+$('.btn-rombo').children().addClass('p_prodotto');
 
 
 $('.seemore').on('click', function() {
-    $('p.base_text').clone().removeClass('base_text').appendTo('.descr_card');
+    $('p.base_text').clone().removeClass('base_text').addClass('prova').appendTo('.descr_card');
     $('.descr_card').addClass('d-block').removeClass('d-none');
 
     $('.sfondo').on('click', function(){
