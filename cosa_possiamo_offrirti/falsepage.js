65c89e74c1d6a983371b4459b8d822872ec15528
nukiniki <157293755+nukiniki@users.noreply.github.com>
2024-01-29 18:29:52 +0100

    pagine complete

--- cosa_possiamo_offrirti/falsepage.js
+++ cosa_possiamo_offrirti/falsepage.js
@@ -1,12 +1,27 @@
-const openpopup = document.getElementById("openpopup")
-const closepopup = document.getElementById("closepopup")
-const popup = document.getElementById("popup")
+document.addEventListener("DOMContentLoaded", () => {
+    const openpopup = document.getElementById("openpopup")
+    const closepopup = document.getElementById("closepopup")
+    const popup = document.getElementById("popup")
+    const fakeimgs = document.querySelectorAll(".fakeimg")
+    const blur = document.querySelector(".blur")
+    const fakelogo = document.querySelector(".fakelogo")
+    const imglogo = document.querySelector(".imglogo")
+    
+    openpopup.addEventListener("click", ()=>{
+        document.documentElement.dataset.theme = ""
+        popup.classList.add("open")
+        blur.style.display="block"
+        blur.classList.add("visible")
+        fakelogo.style.display="none"
+        imglogo.style.display="block"
+        fakeimgs.forEach((fakeimg) => {
+            fakeimg.setAttribute("src",fakeimg.getAttribute("srcalt"))
+        })
+    })
+    
+    closepopup.addEventListener("click", ()=> {
+        popup.classList.remove("open")
+    })
 
 
-openpopup.addEventListener("click", ()=>{
-    popup.classList.add("open")
-})
-
-closepopup.addEventListener("click", ()=> {
-    popup.classList.remove("open")
 })
