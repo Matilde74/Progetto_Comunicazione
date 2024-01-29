3efc562dfbf2e902fce82101233f863fb65b02f8
Mati <m.cotti1@campus.unimib.it>
2024-01-29 22:05:33 +0100

    Sistemazione chi siamo

---
+++ Menu_2.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let open = false
+
+    const nav = document.querySelector("#nav")
+    const width = nav.getBoundingClientRect().width
+
+    const menu = document.querySelector("#menu")
+
+    const blur = document.querySelector("#blur")
+    nav.style.transform= `translateX(-${width}px)`
+
+    menu.addEventListener("click", () => {
+        open = !open
+        menu.classList.toggle ("open")
+        blur.classList.toggle ("visible")
+        if (open) {
+            nav.style.transform= `translateX(0)`
+            console.log(nav.style)
+            blur.style.display="block"
+        }
+        else {
+            nav.style.transform= `translateX(-${width}px)`
+            setTimeout(()=>{blur.style.display="none"},500)
+        }
+    })
+
+})
